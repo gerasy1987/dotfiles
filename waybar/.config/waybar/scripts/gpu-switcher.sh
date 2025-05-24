ed909f9041784ccc124574363c8c506fc1ca8267
gerasy1987 <gs2716@columbia.edu>
2025-05-24 16:53:30 -0500

    minor improvements to waybar and waypaper

--- waybar/.config/waybar/scripts/gpu-switcher.sh
+++ waybar/.config/waybar/scripts/gpu-switcher.sh
@@ -6,7 +6,7 @@ if [ "$current_mode" == "integrated" ]; then
     echo "{\"text\": \"󰿈\", \"tooltip\": \"integrated\"}"
 elif [ "$current_mode" == "hybrid" ]; then
     echo "{\"text\": \"\", \"tooltip\": \"hybrid\"}"
-elif [ "$current_mode" == "" ]; then
+elif [ "$current_mode" == "nvidia" ]; then
     echo "{\"text\": \"\", \"tooltip\": \"nvidia\"}"
 else
     echo "{\"text\": \"GPU Unknown\", \"tooltip\": \"unknown\"}"
